c53985b3205a792cd810e79448778d15c2821fe8
eva <mail@gastrodon.io>
2024-03-19 15:28:17 -0400

    consolodate files
    get rid of model_ files

--- plugin.go
+++ plugin.go
@@ -1,5 +1,27 @@
 package sdk
 
+type kind int
+
+const (
+	PRODUCER    kind = 0b0001
+	CONSUMER    kind = 0b0010
+	TRANSFORMER kind = 0b0100
+)
+
+type Resource struct {
+	Kinds              kind
+	Name               string
+	Spec               SpecMap
+	ProvideProducer    ProducerProvider
+	ProvideConsumer    ConsumerProvider
+	ProvideTransformer TransformerProvider
+}
+
+type Plugin struct {
+	Name      string
+	Resources []*Resource
+}
+
 type Parser func(interface{}) error
 type SpecParser func(SpecMap, interface{}) error
 
