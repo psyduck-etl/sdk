059e967b4803355a1f4f46751b79e2187b1ee2b5
eva <mail@gastrodon.io>
2024-03-25 07:14:53 -0400

    refactor sdk helper spec
    remove everything but per-minute ( that's the only one in use ), and make
    it a uint since negative values make no sense
    
    privatize [PipelineConfig -> sdkConfig] and publicize
    [specPipelineConfig -> PipelineSpec]
    
    drop mustParse and instead return encountered error

--- spec.go
+++ spec.go
@@ -51,12 +51,18 @@ func SpecPerMinute(value int64) *Spec {
 	}
 }
 
-func SpecExitOnError(value bool) *Spec {
-	return &Spec{
-		Name:        "exit-on-error",
-		Description: "stop producing/consuming if we encounter an error",
-		Type:        Bool,
-		Required:    false,
-		Default:     cty.BoolVal(value),
+type sdkConfig struct {
+	PerMinute uint `psy:"per-minute"`
+}
+
+func PipelineSpec() SpecMap {
+	return SpecMap{
+		"per-minute": &Spec{
+			Name:        "per-minute",
+			Description: "target producing/consuming n items per minute ( or 0 for unrestricted )",
+			Type:        Integer,
+			Required:    false,
+			Default:     cty.NumberIntVal(180),
+		},
 	}
 }
