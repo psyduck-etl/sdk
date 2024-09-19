7bc716bfcb5608e8378f0823f9755578943e5b13
eva <mail@gastrodon.io>
2024-09-19 19:14:27 -0400

    plugin ctx
    Create a function for generating the EvalContext of a plugin. Drop
    MoverDesc since we only need the cty value.

--- plugin.go
+++ plugin.go
@@ -1,6 +1,7 @@
 package sdk
 
 import (
+	"github.com/hashicorp/hcl/v2"
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
 )
@@ -13,16 +14,10 @@ const (
 	TRANSFORMER
 )
 
-type MoverDesc struct {
-	Kind     MoverKind
-	Resource string    `hcl:"resource" cty:"resource"`
-	Options  cty.Value `hcl:",remain" cty:"options"`
-}
-
 type Resource struct {
 	Kinds              MoverKind
 	Name               string
-	Spec               SpecMap
+	Spec               []*Spec
 	ProvideProducer    Provider[Producer]
 	ProvideConsumer    Provider[Consumer]
 	ProvideTransformer Provider[Transformer]
@@ -31,7 +26,63 @@ type Resource struct {
 type Plugin struct {
 	Name      string
 	Resources []*Resource
+	Variables map[string]cty.Value
 	Functions map[string]function.Function
+}
+
+func genFunc(res *Resource) function.Function {
+	params := make([]function.Parameter, len(res.Spec))
+	for i, spec := range res.Spec {
+		params[i] = function.Parameter{
+			Name:             spec.Name,
+			Description:      spec.Description,
+			Type:             spec.Type,
+			AllowNull:        false,
+			AllowUnknown:     false,
+			AllowDynamicType: false,
+			AllowMarked:      false,
+		}
+	}
+
+	return function.New(&function.Spec{
+		Description: "Generate a " + res.Name + " resource",
+		Params:      params,
+		Type: func(args []cty.Value) (cty.Type, error) {
+			options := make(map[string]cty.Type, len(args))
+			for i := range len(args) {
+				options[params[i].Name] = params[i].Type
+			}
+
+			return cty.Object(map[string]cty.Type{
+				"resource": cty.String,
+				"options":  cty.Object(options),
+			}), nil
+		},
+		Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
+			options := make(map[string]cty.Value, len(args))
+			for i, arg := range args {
+				options[params[i].Name] = arg
+			}
+
+			return cty.ObjectVal(map[string]cty.Value{
+				"resource": cty.StringVal(res.Name),
+				"options":  cty.ObjectVal(options),
+			}), nil
+		},
+		// TODO RefineResult based on spec defaults / requireds?
+	})
+}
+
+func (p *Plugin) Ctx() *hcl.EvalContext {
+	functions := make(map[string]function.Function, len(p.Resources))
+	for _, res := range p.Resources {
+		functions[res.Name] = genFunc(res)
+	}
+
+	return &hcl.EvalContext{
+		Variables: map[string]cty.Value{},
+		Functions: functions,
+	}
 }
 
 type Parser func(interface{}) error
